ea36c207915ccd93be2a21c4552247eba92d150b
Soikat Hossain <61920251+soikatkhan61@users.noreply.github.com>
2022-04-15 02:38:12 +0600

    Create data_fetch.sh

---
+++ data_fetch.sh
@@ -0,0 +1,453 @@
+#! /bin/bash
+
+#GLOBAL VARIABLES
+$STUDENT_ID
+$PASSWORD
+$STUDENT_NAME
+$IS_LOGGED_IN
+
+#------------------------------------------------
+#Utility Functions
+#------------------------------------------------
+
+#text file read
+readTextFile() {
+    input=./$1
+    while IFS= read -r line; do
+        echo "$line"
+    done <"$input"
+}
+
+#search faculty profile
+searchFacProfile() {
+    fetch_complete=0
+    for ((i = 1; i <= 7; i++)); do
+        if (($i == 1)); then
+            curl -s https://green.edu.bd/faculty-profile-dept/dept-of-cse/ | htmlq --pretty '.profile-faculty' a -a href | uniq >>fac_link.txt
+            fetch_complete=$((fetch_complete + $((100 / 7))))
+            echo "Fetching $fetch_complete %"
+        else
+            curl -s https://green.edu.bd/faculty-profile-dept/dept-of-cse/page/$i | htmlq --pretty '.profile-faculty' a -a href | uniq >>fac_link.txt
+            fetch_complete=$((fetch_complete + $((100 / $i))))
+            echo "Fetching $fetch_complete %"
+        fi
+    done
+}
+
+genarateShortNameList() {
+    echo "Please Wait...Working on generating short list...."
+    for ((i = 1; i <= 7; i++)); do
+        if (($i == 1)); then
+            curl -s https://green.edu.bd/faculty-profile-dept/dept-of-cse/ | htmlq '.faculty-profile' div '.title' --text >>fac.html
+        else
+            curl -s https://green.edu.bd/faculty-profile-dept/dept-of-cse/page/$i | htmlq '.faculty-profile' div '.title' --text >>fac.html
+            echo "Fetching.."
+        fi
+
+        input="./fac.html"
+        while IFS= read -r line; do
+            echo -n "$line = " >>shortList.txt
+            for word in $line; do
+                if [[ $word == "Prof." || $word == "Dr." || $word == "Ms." || $word == "Mr." ]]; then
+                    continue
+                fi
+                echo -n "${word:0:1}" >>shortList.txt
+            done
+
+            echo -e >>shortList.txt
+        done <"$input"
+
+        rm ./fac.html
+    done
+}
+
+#search by short name of a teacher and can be extract all the information about their
+searchByShortName() {
+    read -p "Enter the short form: " name
+
+    line_num=$(grep -n -w "$name" shortList.txt | tr -dc '0-9')
+    link=null
+    FILE=./fac_link.txt
+
+    if test -f "$FILE"; then
+        link=$(sed -n $line_num"p" fac_link.txt)
+    else
+        echo "Fac link not exists"
+        echo "Please Wait! Fething faculty profile..."
+        searchFacProfile
+        link=$(sed -n $line_num"p" fac_link.txt)
+    fi
+    full_name=$(sed -n $line_num"p" shortList.txt | cut -f1 -d"=")
+    echo -e "\n---------------------------------------------"
+    echo -e "\nFull Name: $full_name"
+    echo "Profile Link is: $link"
+
+    #teachers profile download locally if not exists
+    checkTeacherProfileLocally="./assets/teachers/$name.html"
+    if test -f "$checkTeacherProfileLocally"; then
+        echo "All the file are already download in your machine"
+    else
+        curl -s $link >>./assets/teachers/$name.html
+        echo "All the info belongs to $name is downloaded :)) happy scraping !"
+    fi
+    echo -e "\n----------------------------------------------\n"
+
+    #------------------------------------------------
+    #teachers info menu
+    #------------------------------------------------
+    while [ "$choose_option" != 6 ]; do
+        echo -e "\nDo you want to know more about $full_name?"
+        echo "[1]-Education"
+        echo "[2]-Experience"
+        echo "[3]-Publication"
+        echo "[4]-Research"
+        echo "[5]-Back to Teachers Portal"
+
+        read choose_option
+
+        if [ "$choose_option" == 1 ]; then
+            echo "Education Qualification:"
+            echo -e "-------------------------\n\n"
+
+            if test -f "$checkTeacherProfileLocally"; then
+                htmlq -p ".tab-content #tab-education" -t -w <"$checkTeacherProfileLocally" >>edu.txt
+            else
+                curl $link | htmlq -p ".tab-content #tab-education" -t -w >>edu.txt
+            fi
+
+            readTextFile "edu.txt"
+            rm ./edu.txt
+
+        elif [ "$choose_option" == 2 ]; then
+            echo "Experinces"
+            echo -e "-------------------------\n\n"
+
+            if test -f "$checkTeacherProfileLocally"; then
+                htmlq -p ".tab-content #tab-experience" -t -w <"$checkTeacherProfileLocally" >>exp.txt
+            else
+                curl $link | htmlq -p ".tab-content #tab-experience" -t -w >>exp.txt
+            fi
+
+            readTextFile "exp.txt"
+            rm ./exp.txt
+
+        elif [ "$choose_option" == 3 ]; then
+            echo "Publications"
+            echo -e "-------------------------\n\n"
+
+            if test -f "$checkTeacherProfileLocally"; then
+                htmlq -p ".tab-content #tab-publications" -t -w <"$checkTeacherProfileLocally" >>pub.txt
+            else
+                curl $link | htmlq -p ".tab-content #tab-publications" -t -w >>pub.txt
+            fi
+
+            readTextFile "pub.txt"
+            rm ./pub.txt
+
+        #reasearch option starts here
+        elif [ "$choose_option" == 4 ]; then
+            echo "Research"
+            echo -e "-------------------------\n\n"
+
+            if test -f "$checkTeacherProfileLocally"; then
+                htmlq -p ".tab-content #tab-research" -t -w <"$checkTeacherProfileLocally" >>res.txt
+            else
+                curl $link | htmlq -p ".tab-content #tab-research" -t -w >>res.txt
+            fi
+
+            readTextFile "res.txt"
+            rm ./res.txt
+        #reasearch option starts here
+
+        elif [ "$choose_option" == 5 ]; then
+            teacherPortal
+        fi
+
+    done
+}
+
+installDependencies() {
+    wget -O htmlq.tar.gz https://github.com/mgdm/htmlq/releases/latest/download/htmlq-x86_64-linux.tar.gz
+    sudo tar xf htmlq.tar.gz -C /usr/local/bin
+    htmlq --version
+}
+
+teacherPortal() {
+
+    echo -e "\n[1]- Get short-form list"
+    echo "[2]- Search by short name"
+    echo "[3]- Back"
+    read option
+    if [ "$option" == 1 ]; then
+        genarateShortNameList
+        readTextFile "shortList.txt"
+    elif [ "$option" == 2 ]; then
+        searchByShortName
+    elif [ "$option" == 3 ]; then
+        main_menu
+    fi
+
+}
+
+#---------------------------------------
+#End of teachers section
+#----------------------------------------
+deleteCachedFile(){
+        #remove everything belonsg if cached file exists
+    checkProfileHtml="./profile.html"
+    if test -f "$checkProfileHtml"; then
+        rm ./profile.html
+    fi
+
+    checkResultHtml="./result.html"
+    if test -f "$checkResultHtml"; then
+        rm ./result.html
+    fi
+
+    checkBilltHtml="./bill.html"
+    if test -f "$checkBilltHtml"; then
+        rm ./bill.html
+    fi
+
+    checkProfile_info="./profile.txt"
+    if test -f "$checkProfile_info"; then
+        rm ./profile.txt
+    fi
+}
+
+checkBillingStatus() {
+    if [ -z "${STUDENT_ID}" ] && [ -z "${PASSWORD}" ]; then
+        echo "You dont set your student id,password yet! "
+        studentPortalLogin
+    else
+
+        checkBillFileExists=./bill.html
+
+        if test -f "$checkBillFileExists"; then
+            echo -e "\n..................................................\n"
+            htmlq -p "#cleContent .uwp-h2 font" --text <bill.html
+            htmlq -p "#cleContent  .uwp-noborder" -w --text <bill.html
+            echo -e "..................................................\n"
+        else
+            echo -e "\n................................................."
+            echo "Extracting bill status..."
+            curl -s -X POST -F "studentID=$STUDENT_ID" -F "password=$PASSWORD" http://portal.green.edu.bd:81/Billing.php >>bill.html
+
+            echo -e "\n..................................................\n"
+            htmlq -p "#cleContent .uwp-h2 font" --text <bill.html
+            htmlq -p "#cleContent  .uwp-noborder" -w --text <bill.html
+            echo -e "..................................................\n"
+        fi
+        studentPortal
+    fi
+
+}
+
+checkResult() {
+
+    askForFullResult() {
+        read -p "Want to get full result[y/n]" ans
+        if [ $ans == "Y" ] || [ $ans == "y" ]; then
+            htmlq -p "#rest #cleContent table" -w --text <result.html
+        fi
+    }
+
+    if [ -z "${STUDENT_ID}" ] && [ -z "${PASSWORD}" ]; then
+        echo "You dont set your student id,password yet! "
+        studentPortalLogin
+    else
+
+        checkResultExists=./result.html
+
+        if test -f "$checkResultExists"; then
+            echo -e "\n..................................................\n"
+            htmlq -p ".uwp-student-info" -t <result.html
+            echo -e "..................................................\n"
+            htmlq -p "#rest  #cleContent .uwp-h2" -w -t <result.html
+            echo -e "..................................................\n"
+            askForFullResult
+        else
+            echo -e "\n................................................."
+            echo "Extracting your result ..."
+            curl -s -X POST -F "studentID=$STUDENT_ID" -F "password=$PASSWORD" http://portal.green.edu.bd:81/Status.php >>result.html
+
+            echo -e "\n..................................................\n"
+            htmlq -p ".uwp-student-info" -t <result.html
+            echo -e "..................................................\n"
+            htmlq -p "#rest  #cleContent .uwp-h2" -w -t <result.html
+            echo -e "..................................................\n"
+            askForFullResult
+        fi
+        studentPortal
+    fi
+}
+
+profileInfoExtract() {
+    if [ -z "${STUDENT_ID}" ] && [ -z "${PASSWORD}" ]; then
+        echo "You dont set your student id,password yet! "
+        studentPortalLogin
+    else
+
+        checkProfileExists=./profile.html
+
+        if test -f "$checkProfileExists"; then
+            echo -e "\n..................................................\n"
+            htmlq -p "#cleContent tr" -w -t <profile.html
+            echo -e "..................................................\n"
+        else
+            echo -e "\n................................................."
+            echo "Extracting profile information..."
+            curl -s -X POST -F "studentID=$STUDENT_ID" -F "password=$PASSWORD" http://portal.green.edu.bd:81/profile.php >>bill.html
+
+            echo -e "\n..................................................\n"
+            htmlq -p "#cleContent tr" -w -t <profile.html
+            echo -e "..................................................\n"
+        fi
+        studentPortal
+    fi
+}
+studentPortalLogin() {
+
+    deleteCachedFile
+
+    read -p "Enter your student id: " STUDENT_ID
+    read -s -p "Enter your password(it will be shaded, dont worry!): " PASSWORD
+    echo -e "\n.............................."
+    #extracting student name
+    curl -s -X POST -F "studentID=$STUDENT_ID" -F "password=$PASSWORD" http://portal.green.edu.bd:81/profile.php >>profile.html
+    htmlq -p "#cleContent tr" -w -t <profile.html >>profile.txt
+    STUDENT_NAME=$(sed -n 3p profile.txt)
+    #extracting student name ends here
+    echo "ID and Password is set!"
+    echo ".............................."
+    IS_LOGGED_IN="true"
+    echo $IS_LOGGED_IN
+    studentPortal
+}
+
+
+
+studentPortalLogout() {
+    IS_LOGGED_IN="false"
+    STUDENT_NAME=""
+    STUDENT_ID=""
+    PASSWORD=""
+
+    #remove everything belonsg to logged id
+    checkProfileHtml="./profile.html"
+    if test -f "$checkProfileHtml"; then
+        echo "Deleting Profile..."
+        rm ./profile.html
+    else
+        echo "Deleting Profile..."
+    fi
+
+    checkResultHtml="./result.html"
+    if test -f "$checkResultHtml"; then
+        echo "Deleting result.html..."
+        rm ./result.html
+    else
+        echo "Deleting result.html..."
+    fi
+
+    checkBilltHtml="./bill.html"
+    if test -f "$checkBilltHtml"; then
+        echo "Deleting bill.html..."
+        rm ./bill.html
+    else
+        echo "Deleting bill.html..."
+    fi
+
+    checkProfile_info="./profile.txt"
+    if test -f "$checkProfile_info"; then
+        echo "Deleting profile.txt..."
+        rm ./profile.txt
+    else
+        echo "Deleting profile.txt..."
+    fi
+
+    studentPortal
+}
+
+checkLoggedIn() {
+    if [ -z "${STUDENT_ID}" ] && [ -z "${PASSWORD}" ]; then
+        echo "You dont set your student id,password yet! "
+        studentPortalLogin
+    fi
+}
+
+studentPortal() {
+
+    echo -e "\n"
+    echo "[1]-Billing Status"
+    echo "[2]-Result"
+    echo "[3]-Profile"
+    if [ -z "${STUDENT_ID}" ] && [ -z "${PASSWORD}" ]; then
+        echo "[4]-Login"
+    else
+        echo "[4]-Welcome, $STUDENT_NAME! Want to Logout?"
+    fi
+
+    echo "[5]-Back"
+    read option
+
+    if [ "$option" == 1 ]; then
+        checkBillingStatus
+    elif [ "$option" == 2 ]; then
+        checkResult
+    elif [ "$option" == 3 ]; then
+        profileInfoExtract
+    elif [ "$option" == 4 ]; then
+        if [ "$IS_LOGGED_IN" = "true" ]; then
+            studentPortalLogout
+        else
+            studentPortalLogin
+        fi
+    elif [ "$option" == 5 ]; then
+        main_menu
+    fi
+}
+#-------------------------------------------------
+#main program start here
+#-------------------------------------------------
+if [ ! -d "assets" ]; then
+    mkdir assets
+    mkdir -p assets/teachers
+fi
+
+playground() {
+    echo "Md. Abu Rumman Refat = MARR" | cut -f1 -d"="
+}
+
+main_menu() {
+    echo -e "\n[1]-Teacher Information"
+    echo "[2]-Student Portal"
+    echo "[3]-Install Dependecies"
+    echo "[4]-Generate Faculty Profile link"
+    echo "[5]-Exit"
+    echo "[6]-Playground"
+    read main_menu
+
+    if [ "$main_menu" == 1 ]; then
+        teacherPortal
+    elif [ "$main_menu" == 2 ]; then
+        studentPortal
+    elif [ "$main_menu" == 3 ]; then
+        installDependencies
+    elif [ "$main_menu" == 4 ]; then
+        searchFacProfile
+    elif [ "$main_menu" == 6 ]; then
+        playground
+    elif [ "$main_menu" == 5 ]; then
+        #remove everything belonsg if logged in
+        deleteCachedFile
+
+        echo "Happy scraping! Thank You for using me <3 "
+        exit
+    fi
+}
+
+main_menu
+#-------------------------------------------------
+#main program ends here
+#-------------------------------------------------
